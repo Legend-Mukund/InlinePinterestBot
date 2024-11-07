721954bdf071cc18293c0936d9b1385feb23d807
Mukund <mukundsrajput@gmail.com>
2024-11-07 15:34:49 +0530

    markdown error fixed

--- internals/methods/start.go
+++ internals/methods/start.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Start(b *gotgbot.Bot, ctx *ext.Context) error {
-	message := "Hello **%s** there i am **Pinterest** bot to send images from pinterest to your friends instantly in the chat"
+	message := "Hello *%s* there i am *Pinterest* bot to send images from pinterest to your friends instantly in the chat"
 	_, err := ctx.EffectiveMessage.Reply(b, fmt.Sprintf(message, ctx.EffectiveUser.FirstName), &gotgbot.SendMessageOpts{
 		ParseMode: "MARKDOWN",
 	})
