894e8d635efe4d75d24949f52199dd86fac05d41
Mukund <mukundsrajput@gmail.com>
2025-02-02 14:36:19 +0530

    feat: Implement force subscriptions, remove SQL dependency

--- internals/methods/pin.go
+++ internals/methods/pin.go
@@ -3,6 +3,7 @@ package methods
 import (
 	"fmt"
 	"math/rand"
+	"slices"
 
 	"github.com/MukundSinghRajput/InlinePinterestBot/internals/api"
 	"github.com/PaulSonOfLars/gotgbot/v2"
@@ -11,8 +12,98 @@ import (
 
 func Pin(b *gotgbot.Bot, ctx *ext.Context) error {
 	query := ctx.InlineQuery.Query
+	emptyText := " "
+
+	cm, err := b.GetChatMember(-1002146498946, ctx.InlineQuery.From.Id, &gotgbot.GetChatMemberOpts{})
+
+	if err != nil {
+		_, err := ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
+			gotgbot.InlineQueryResultArticle{
+				Id:    fmt.Sprintf("%d", rand.Int()),
+				Title: "Errorr !!!",
+				InputMessageContent: gotgbot.InputTextMessageContent{
+					MessageText: fmt.Sprintf("An error occured: %v", err),
+				},
+				Description: "An error occured",
+				ReplyMarkup: &gotgbot.InlineKeyboardMarkup{
+					InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+						{Text: "Search", SwitchInlineQuery: &emptyText},
+					}},
+				},
+			},
+		}, &gotgbot.AnswerInlineQueryOpts{
+			CacheTime:  0,
+			IsPersonal: true,
+		})
+		return err
+	}
+
+	if cm.GetStatus() == "kicked" {
+		_, err := ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
+			gotgbot.InlineQueryResultArticle{
+				Id:    fmt.Sprintf("%d", rand.Int()),
+				Title: "Banned !!!",
+				InputMessageContent: gotgbot.InputTextMessageContent{
+					MessageText: "Sadly you have been banned from @MukundX therefore you can't use me.",
+				},
+				Description: "You have been banned from @MukundX",
+				ReplyMarkup: &gotgbot.InlineKeyboardMarkup{
+					InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+						{Text: "Report", Url: "https://t.me/MukundSRajput?text=Hey+I+have+been+banned+from+your+channel+@MukundX+look+into+this+maybe+it's+just+a+mistake"},
+					}},
+				},
+			},
+		}, &gotgbot.AnswerInlineQueryOpts{
+			CacheTime:  0,
+			IsPersonal: true,
+		})
+		return err
+	}
+
+	x := []string{"creator", "administrator", "member"}
+
+	if !slices.Contains(x, cm.GetStatus()) {
+		_, err := ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
+			gotgbot.InlineQueryResultArticle{
+				Id:    fmt.Sprintf("%d", rand.Int()),
+				Title: "Ehhhh !!!",
+				InputMessageContent: gotgbot.InputTextMessageContent{
+					MessageText: "Join @MukundX to use me",
+				},
+				Description: "Join @MukundX first",
+				ReplyMarkup: &gotgbot.InlineKeyboardMarkup{
+					InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+						{Text: "Join", Url: "https://t.me/MukundX"},
+					}},
+				},
+			},
+		}, &gotgbot.AnswerInlineQueryOpts{
+			CacheTime:  0,
+			IsPersonal: true,
+		})
+		return err
+	}
+
 	if query == "" {
-		return nil
+		_, err := ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
+			gotgbot.InlineQueryResultArticle{
+				Id:    fmt.Sprintf("%d", rand.Int()),
+				Title: "No query",
+				InputMessageContent: gotgbot.InputTextMessageContent{
+					MessageText: "Please enter a query to search for",
+				},
+				Description: "Please enter a query to search for",
+				ReplyMarkup: &gotgbot.InlineKeyboardMarkup{
+					InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+						{Text: "Search", SwitchInlineQuery: &emptyText},
+					}},
+				},
+			},
+		}, &gotgbot.AnswerInlineQueryOpts{
+			CacheTime:  0,
+			IsPersonal: true,
+		})
+		return err
 	}
 
 	p := api.NewPinterest()
@@ -32,6 +123,11 @@ func Pin(b *gotgbot.Bot, ctx *ext.Context) error {
 					LinkPreviewOptions: &gotgbot.LinkPreviewOptions{
 						IsDisabled: true,
 					},
+				},
+				ReplyMarkup: &gotgbot.InlineKeyboardMarkup{
+					InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+						{Text: "Search", SwitchInlineQuery: &emptyText},
+					}},
 				},
 				Description: fmt.Sprintf("The query %s wasn't found", query),
 			},
@@ -53,14 +149,35 @@ func Pin(b *gotgbot.Bot, ctx *ext.Context) error {
 			ThumbnailUrl: imageURL,
 			Title:        fmt.Sprintf("Image %d", i+1),
 			Description:  fmt.Sprintf("Pinterest Image %d", i+1),
+			ReplyMarkup: &gotgbot.InlineKeyboardMarkup{
+				InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+					{Text: "Search Again", SwitchInlineQuery: &emptyText},
+				}},
+			},
 		})
 	}
 
-	_, err = ctx.InlineQuery.Answer(b, articles, &gotgbot.AnswerInlineQueryOpts{
-		IsPersonal: true,
-	})
+	_, err = ctx.InlineQuery.Answer(b, articles, &gotgbot.AnswerInlineQueryOpts{})
+
 	if err != nil {
-		return fmt.Errorf("failed to send inline query results: %w", err)
+		_, err := ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
+			gotgbot.InlineQueryResultArticle{
+				Id:    fmt.Sprintf("%d", rand.Int()),
+				Title: "Errorr !!!",
+				InputMessageContent: gotgbot.InputTextMessageContent{
+					MessageText: fmt.Sprintf("Some error occured while fetching the images while searching for %v", query),
+				},
+				Description: "Some error occured while fetching the images",
+				ReplyMarkup: &gotgbot.InlineKeyboardMarkup{
+					InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+						{Text: "Search", SwitchInlineQuery: &emptyText},
+					}},
+				},
+			},
+		}, &gotgbot.AnswerInlineQueryOpts{
+			CacheTime: 0,
+		})
+		return err
 	}
 	return nil
 }
